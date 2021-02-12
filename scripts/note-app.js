88e7813a0403aa1e59f571f6d7852fc3b7e378e0
Paulo Almeida <paulo.roberto.a@hotmail.com>
2021-02-12 23:34:26 +0100

    notes app final

--- scripts/note-app.js
+++ scripts/note-app.js
@@ -4,3 +4,35 @@ const filters = {
     searchText: "",
     sortBy: "byEdited"
 };
+
+renderNotes(notes, filters);
+"use strict"
+
+dpcument.querySelector("#create-note").addEventListener("click", () => {
+    const id = uuidv4();
+    notes.push({
+        id: id,
+        title: "",
+        body: "",
+        createdAt: timeStamp,
+        updatedAt: timeStamp,
+    }),
+    saveNotes(notes);
+    location.assign("./edit.html#{id}");
+});
+
+document.querySelector("#seach-text").addEventListener("input", (e) => {
+    filters.searchText = e.target.value;
+    renderNotes(notes, filters);
+});
+document.querySelector("#filter-by").addEventListener("change", (e) => {
+    filters.sortBy = e.target.value;
+    renderNotes(notes, filters);
+})
+
+window.addEventListener("storage", (e) => {
+    if (e.key === "notes"){
+        notes = JSON.parse(e.newValue);
+        renderNotes(notes, filters);
+    }
+})
